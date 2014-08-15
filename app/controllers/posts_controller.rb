62893028f29ec30757ac42a5cd0fc9a3f8b321bf
Nick Stewart <nicholas4@gmail.com>
2014-08-15 09:58:19 -0600

    Add where constraint to posts retrieval

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order("points DESC, created_at DESC")
+    @posts = Post.where(created_at: (24.hours.ago..Time.now)).order("points DESC, created_at DESC")
   end
 end
