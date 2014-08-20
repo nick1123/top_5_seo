1e8078843f703f040eafccd7afefc8857e67915f
Nick Stewart <nicholas4@gmail.com>
2014-08-19 19:47:48 -0600

    rm ip tracking

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,31 +5,34 @@ class Post < ActiveRecord::Base
   def increment_clicks(remote_ip)
     self.clicks += 1
 
-    if !self.click_ip_addresses.to_s.include?(remote_ip)
-      self.click_ip_addresses = self.click_ip_addresses.to_s + remote_ip + ' '
-      self.points += 1
-    end
+#    if !self.click_ip_addresses.to_s.include?(remote_ip)
+#      self.click_ip_addresses = self.click_ip_addresses.to_s + remote_ip + ' '
+#      self.points += 1
+#    end
 
+    self.points += 1
     self.save
   end
 
   def vote_up(remote_ip)
     self.votes_up += 1
-    if !voted_before?(remote_ip)
-      record_vote_ip(remote_ip)
-      self.points += POINTS_PER_VOTE
-    end
+#    if !voted_before?(remote_ip)
+#      record_vote_ip(remote_ip)
+#      self.points += POINTS_PER_VOTE
+#    end
 
+    self.points += POINTS_PER_VOTE
     self.save
   end
 
   def vote_down(remote_ip)
     self.votes_down += 1
-    if !voted_before?(remote_ip)
-      record_vote_ip(remote_ip)
-      self.points -= POINTS_PER_VOTE
-    end
+#    if !voted_before?(remote_ip)
+#      record_vote_ip(remote_ip)
+#      self.points -= POINTS_PER_VOTE
+#    end
 
+    self.points -= POINTS_PER_VOTE
     self.save
   end
 
